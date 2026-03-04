7744da87743c401d61287a89e611bcf4c3bc05bc
David Parker <nexai.coach@gmail.com>
2026-03-04 22:31:53 +0400

    Deleted settings.json

--- .next/server/app/api/create-checkout-session/route.js
+++
@@ -1,9 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/create-checkout-session/route.js")
-R.c("server/chunks/[root-of-the-server]__86a8fe29._.js")
-R.c("server/chunks/node_modules_next_dist_92160604._.js")
-R.c("server/chunks/[root-of-the-server]__1130533d._.js")
-R.c("server/chunks/[root-of-the-server]__c14f5232._.js")
-R.c("server/chunks/node_modules_next_dist_79f1aee4._.js")
-R.c("server/chunks/_next-internal_server_app_api_create-checkout-session_route_actions_6969ce6c.js")
-R.m(3600)
-module.exports=R.m(3600).exports
--- .next/server/middleware.js
+++
@@ -1,6 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("server/middleware.js")
-R.c("server/chunks/[root-of-the-server]__008b6a17._.js")
-R.c("server/chunks/[root-of-the-server]__6a9a3fe0._.js")
-R.c("server/chunks/node_modules_next_dist_03fe560d._.js")
-R.m(62395)
-module.exports=R.m(62395).exports
